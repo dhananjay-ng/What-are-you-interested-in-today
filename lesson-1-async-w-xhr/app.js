fe4bf5fe58ff43574bf5df3e860e7a0c5d82cdc2
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-11-22 13:39:52 +0530

    image search is workingt

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -4,9 +4,36 @@
     let searchedForText;
     const responseContainer = document.querySelector('#response-container');
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
-    });
+    function addImage() {
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText)
+        if (data != null && data.results[0] && data.results.length > 0) {
+            let firstImage = data.results[0];
+            htmlContent = `<figure>
+            <img src="${firstImage.urls.regular}" alt="${searchedForText} "/>
+            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+
+        } else {
+            htmlContent = `<div class="error-no-image">No Images Available</div>`;
+        }
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    form
+        .addEventListener('submit', function (e) {
+            e.preventDefault();
+            responseContainer.innerHTML = '';
+            searchedForText = searchField.value;
+
+            const unsplashRequest = new XMLHttpRequest();
+
+            unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+            unsplashRequest.setRequestHeader('Authorization', 'Client-ID 102774a71dda22ec30ac4724bef425375806127558ae7bd1c999a31e7b9e7181')
+            unsplashRequest.onload = addImage;
+
+            unsplashRequest.send()
+
+        });
+
 })();
